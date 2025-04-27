bf518bb77617cb23ddc9a4c46e634b4fa5df6a28
Osman Zakaria <111977656+osmanzakaria532@users.noreply.github.com>
2025-04-27 06:32:14 +0600

    some menu item created

---
+++ src/components/section-layout/LeftSideMenu.jsx
@@ -0,0 +1,90 @@
+import { Link } from "react-router-dom";
+
+import { FaUserFriends } from "react-icons/fa";
+import { MdVerified } from "react-icons/md";
+import { MdDashboard } from "react-icons/md";
+
+import p_images from "../../../src/assets/images/p_img.jpg";
+import Flex from "../ui-layout/Flex";
+
+const LeftSideMenu = () => {
+  return (
+    <>
+      <div className="w-[285px] pt-3">
+        <ul>
+          <li className="hover:bg-[#333334] p-2 rounded-md">
+            <Link>
+              <Flex className="gap-3 items-center !justify-start">
+                <div className="w-[12%]">
+                  <div className="w-7">
+                    <img
+                      src={p_images}
+                      alt=""
+                      className="rounded-full"
+                    />
+                  </div>
+                </div>
+                <h2 className="text-white/90 text-sm font-semibold">
+                  Osman Zakaria
+                </h2>
+              </Flex>
+            </Link>
+          </li>
+          <li className="hover:bg-[#333334] p-2 rounded-md">
+            <Link>
+              <Flex className="gap-3 items-center !justify-start">
+                <div className="w-[12%]">
+                  <div className="">
+                    <FaUserFriends className="text-2xl text-white" />
+                  </div>
+                </div>
+                <h2 className="text-white/90 text-sm font-semibold">Friends</h2>
+              </Flex>
+            </Link>
+          </li>
+          <li className="hover:bg-[#333334] p-2 rounded-md">
+            <Link>
+              <Flex className="gap-3 items-center !justify-start">
+                <div className="w-[12%]">
+                  <div className="">
+                    <MdVerified className="text-2xl text-blue-600" />
+                  </div>
+                </div>
+                <h2 className="text-white/90 text-sm font-semibold">
+                  Meta Verified
+                </h2>
+              </Flex>
+            </Link>
+          </li>
+          <li className="hover:bg-[#333334] p-2 rounded-md">
+            <Link>
+              <Flex className="gap-3 items-center !justify-start">
+                <div className="w-[12%]">
+                  <div className="">
+                    <MdDashboard className="text-2xl text-blue-600" />
+                  </div>
+                </div>
+                <h2 className="text-white/90 text-sm font-semibold">
+                  Professional dashboard
+                </h2>
+              </Flex>
+            </Link>
+          </li>
+        </ul>
+
+        {/*  */}
+        <div className="pt-3 pb-5 px-2">
+          <hr className="text-white/90" />
+        </div>
+
+        {/*  */}
+        <ul>
+          <h4 className="text-white/70">Your shortcuts</h4>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default LeftSideMenu;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
+import LeftSideMenu from "../components/section-layout/LeftSideMenu";
 import TopMenuArea from "../components/section-layout/TopMenuArea";
+
+import Flex from "../components/ui-layout/Flex";
 
 const Home = () => {
   return (
     <>
       <TopMenuArea />
+      <Flex className="items-start px-3 bg-[#252728] ">
+        <LeftSideMenu />
+        <div className="">2</div>
+        <div className="">3</div>
+      </Flex>
     </>
   );
 };
