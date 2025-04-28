5078206525116573c6cc43900be0f1ab7c427016
Osman Zakaria <111977656+osmanzakaria532@users.noreply.github.com>
2025-04-28 10:03:03 +0600

    add scrollbar in left sidebar finaly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <TopMenuArea />
       </div>
       <Flex className="items-start px-3 bg-[#252728] ">
-        <div className="">
+        <div className="mt-16 h-[calc(100vh-50px)] overflow-y-auto scrollbar-hide hover:scrollbar-default sticky">
           <LeftSideMenu />
         </div>
         <div className="">2</div>
